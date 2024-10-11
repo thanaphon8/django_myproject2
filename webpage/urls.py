d2872cee5a86160679e8c6e45cb054fb8d54b444
Thanaphon <dthanaphon21@gmail.com>
2024-10-11 09:23:44 +0700

    loginPage

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path("",  views.indexPage, name="index"),
+    path("", views.indexPage, name="index"),
     path("about/", views.aboutUs, name="about"),
     path("contact/", views.contactUs),
     path("for/", views.forPage, name="for-page"),
+    path("login/", views.loginPage, name="login"),
     path("card/", views.cardView, name="card-page"),
     path("color/", views.cardColor, name="card-color"),
     path("forggg/", views.forGGG, name="for-test"),
--- webpage/views.py
+++ webpage/views.py
@@ -11,6 +11,10 @@ def aboutUs(request):
     return render(request, 'about.html')
 
 
+def loginPage(request):
+    return render(request, 'login.html')
+
+
 def contactUs(request):
     return render(request, 'contact.html')
 
